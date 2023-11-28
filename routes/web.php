58cb997a8b526a15600148b270f04eac89a36191
Guilherme Scholl <108899269+GScholl@users.noreply.github.com>
2023-11-28 11:13:58 -0300

    Adição do menu

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('menu');
 });
 
 use App\Http\Controllers\UsuarioController;
